6c0cd0558001c14c3deb6505e9f0fd0a83724395
ngugiauraa <ngugigichuki21@gmail.com>
2026-04-25 18:05:59 +0300

    Fingers Crossed

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import React from 'react';
+
 import { useEffect, useState } from 'react';
 import { useOrganization, useUser } from '@clerk/nextjs';
 import { toast } from 'sonner';
